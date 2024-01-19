1a91f92d01f362f1bd6cf2c143edde5423edb7ff
Eilgnaw <wzqwzq666@hotmail.com>
2024-01-20 01:52:49 +0800

    init

--- Package.swift
+++ Package.swift
@@ -5,19 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "SQLiteCipher",
+    platforms: [
+        .iOS(.v12), .watchOS(.v6),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SQLiteCipher",
-            targets: ["SQLiteCipher"]),
+        .library(name: "SQLiteCipher", targets: ["SQLite", "SQLCipher"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SQLiteCipher"),
-        .testTarget(
-            name: "SQLiteCipherTests",
-            dependencies: ["SQLiteCipher"]),
+        .binaryTarget(
+            name: "SQLite",
+            url: "https://github.com/Eilgnaw/SQLiteCipher/releases/download/0.14.1/SQLite.xcframework.zip",
+            checksum: "ba497ab9ad8ef3eac4bdf4797830680dd3eb9a90fd3abd68c288cc36e755e737"
+        ),
+
+        .binaryTarget(
+            name: "SQLCipher",
+            url: "https://github.com/Eilgnaw/SQLiteCipher/releases/download/0.14.1/SQLCipher.xcframework.zip",
+            checksum: "05752f9ca2919026907f1409c190f0e39060736da83dbe2f2043e19a854021e2"
+        ),
     ]
 )
